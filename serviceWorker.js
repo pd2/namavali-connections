6efbe94ecfca548e2237f9ce9b949972b0c76823
Glitch (namavali-game) <none>
2025-05-26 08:47:54 +0000

    🐳🚂 Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDev = "namavali_game"
+const service_worker_version = "v2.1"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
